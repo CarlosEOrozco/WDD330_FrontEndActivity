f42c08d7b7b333a6cbb4e5cc4543b7f2463b3c83
Carlos Orozco <carlosenmanuelorozcon@gmail.com>
2025-01-23 11:37:26 -0300

    individual 3 up to step 2 completed

--- src/js/main.js
+++ src/js/main.js
@@ -19,4 +19,37 @@ const tentsListing = new ProductListing(
   hiddenTentIds,
 );
 
+//Categories Listing
+document.addEventListener("DOMContentLoaded", () => {
+  const categories = [
+    { name: "Tents", icon: "https://byui-cit.github.io/wdd330/images/category-icons/noun_Tent_3544767.svg", alt: "Tent Icon from Noun Project: Mustofa Bayu", category: "tents" },
+    { name: "Sleeping Bag", icon: "https://byui-cit.github.io/wdd330/images/category-icons/noun_Sleeping%20Bag_3544775.svg", alt: "Sleeping Bag Icon from Noun Project: Mustofa Bayu", category: "sleeping-bags" },
+    { name: "Backpack", icon: "https://byui-cit.github.io/wdd330/images/category-icons/noun_carrier%20bag_3544771.svg", alt: "Backpack Icon from Noun Project: Mustofa Bayu", category: "backpacks" },
+    { name: "Hammock", icon: "https://byui-cit.github.io/wdd330/images/category-icons/noun_Hammock_791143.svg", alt: "Hammock Icon from Noun Project: Paul Richard", category: "hammocks" }
+  ];
+
+  const categoryGrid = document.getElementById("category-grid");
+
+  categories.forEach(category => {
+    const categoryBox = document.createElement("div");
+    categoryBox.classList.add("category-box");
+
+    const categoryLink = document.createElement("a");
+    categoryLink.href = `product-listing/index.html?category=${category.category}`;
+
+    const categoryImg = document.createElement("img");
+    categoryImg.src = category.icon;
+    categoryImg.alt = category.alt;
+
+    const categoryText = document.createElement("p");
+    categoryText.textContent = category.name;
+
+    categoryLink.appendChild(categoryImg);
+    categoryLink.appendChild(categoryText);
+    categoryBox.appendChild(categoryLink);
+    categoryGrid.appendChild(categoryBox);
+  });
+});
+
+
 tentsListing.init();
