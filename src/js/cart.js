f77f4270afaaf88d60888eeac598941201737b6f
AaronCDee <101570796+AaronCDee@users.noreply.github.com>
2025-01-25 13:44:03 +0200

    15 remove from cart feature (#16)
    * feat: add styling for remove
    
    * feat: add remover
    
    * refactor: move to a function
    
    ---------

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,25 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import { loadHeaderFooter } from './utils.mjs';
 import { renderCartSuperScript } from './cartSuperscript.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadHeaderFooter(renderCartSuperScript);
+  loadHeaderFooter(renderCartSuperScript);
+
+  const removeButtons = document.querySelectorAll(".cart-remove-button");
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", removeFromCart);
   });
+});
+
+function removeFromCart(e) {
+  const id        = e.target.dataset.id;
+  const cartItems = getLocalStorage("so-cart") || [];
+  const newCart   = cartItems.filter((item) => item.Id !== id);
+
+  setLocalStorage("so-cart", newCart);
+  renderCartContents();
+  renderCartSuperScript();
+}
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
@@ -13,20 +28,23 @@ function renderCartContents() {
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  const newItem = `<div class="cart-item-container">
+                    <li class="cart-card divider">
+                      <a href="#" class="cart-card__image">
+                        <img
+                          src="${item.Image}"
+                          alt="${item.Name}"
+                        />
+                      </a>
+                      <a href="#">
+                        <h2 class="card__name">${item.Name}</h2>
+                      </a>
+                      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+                      <p class="cart-card__quantity">qty: 1</p>
+                      <p class="cart-card__price">$${item.FinalPrice}</p>
+                    </li>
+                    <button class="cart-remove-button" data-id="${item.Id}">Remove</button>
+                  </div>`;
 
   return newItem;
 }
